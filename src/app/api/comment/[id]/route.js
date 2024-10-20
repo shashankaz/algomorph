3a4a9763ae8f96006fddf0449bc1f482fdcaabfd
Shashank <113260615+shashankaz@users.noreply.github.com>
2024-10-20 12:06:28 +0530

    Refactor PrismaClient initialization in comment route

--- src/app/api/comment/[id]/route.js
+++ src/app/api/comment/[id]/route.js
@@ -1,9 +1,7 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+const prisma = new PrismaClient();
 
 export const GET = async (request) => {
   try {
